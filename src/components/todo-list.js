e8de876b16adddc80a9a4c56335bcf1fc0827652
Павел Хрусталев <Pavel@MacBook-Air-Pavel.local>
2019-01-07 16:52:35 +0300

    props-and-arrays

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+    const elements = todos.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={item.id}
+            ><TodoListItem {...itemProps} />
+            </li>
+        );
+    });
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+        { elements }
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
 const App = () => {
+    const todoData = [
+        { label: 'Drink cofee', important: false, id: 1 },
+        { label: 'Make awesome app', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ];
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData}/>
         </div>
     );
 };
