07a4106a66a0596b914ccfbbb9f419a45414466b
johnconan <johnconan@mail.ru>
2019-01-07 21:58:34 +0300

    structure-ready

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+            className="search-input"
+            placeholder="Type to search"/>
+    );
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
     const todoData = [
@@ -12,12 +15,16 @@ const App = () => {
         { label: 'Have a lunch', important: false, id: 3 }
     ];
     return (
-        <div>
-            <AppHeader />
+        <div className="todo-app">
+          <AppHeader toDo={1} done={3} />
+          <div className="top-panel d-flex">
             <SearchPanel />
-            <TodoList todos={todoData}/>
+            <ItemStatusFilter />
+          </div>
+    
+          <TodoList todos={todoData} />
         </div>
-    );
+      );
 };
 
 
